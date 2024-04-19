62afb2273f03a3c5808047ac0fb2577e7a725904
akshayviola <akshayviola201@gmail.com>
2024-04-19 14:48:13 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wrapper">
+        <div id="rocket"></div>
+        <div class="rocket__body__part">
+          <p>N A S A</p>
+        </div>
+        <div id="advanced" class="circlee"></div>
+        <div id="advancedd" class="circleee"></div>
+        <div class="container">
+          <div class="red flame"></div>
+          <div class="orange flame"></div>
+          <div class="yellow flame"></div>
+          <div class="white flame"></div>
+          <div class="blue circle"></div>
+          <div class="black circle"></div>
+        </div>
+        <div class="wings">
+          <div id="left"></div>
+          <div id="right"></div>
+        </div>
+      </div>
     </div>
   );
 }
